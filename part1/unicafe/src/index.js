29961a6797f472a5b513646bcf50ea7cc4a76935
Pablo Castro <pablocastro1992@hotmail.com>
2019-07-08 19:30:45 -0500

    Finished unicafe 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -17,19 +17,44 @@ const Button = (props) => {
     )
 }
 
+//Check if feedback has been given
+const History = (props) => {
+    if (props.all === 0) {
+        return (
+            <p>No feedback given</p>
+        )
+    }
+
+    return (
+        <div>
+            <Statistics array={props.array} />
+        </div>
+    )
+}
+
+const Statistic = (props) => {
+    return (
+        <tr>
+            <td>{props.text}</td>
+            <td>{props.value}</td>
+        </tr>
+    )
+}
+
 const Statistics = (props) => {
     return (
         <div>
             <h1>Statistics</h1>
-            <p>
-                Good: {props.good}
-            </p>
-            <p>
-                Neutral: {props.neutral}
-            </p>
-            <p>
-                Bad: {props.bad}
-            </p>
+            <table>
+                <tbody>
+                    <Statistic text="Good" value={props.array[0]} />
+                    <Statistic text="Neutral" value={props.array[1]} />
+                    <Statistic text="Bad" value={props.array[2]} />
+                    <Statistic text="All" value={props.array[3]} />
+                    <Statistic text="Average" value={props.array[4]} />
+                    <Statistic text="Positive" value={props.array[5]} />
+                </tbody>
+            </table>
         </div>
     )
 }
@@ -39,6 +64,10 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const all = good + neutral + bad
+    const average = ((good - bad) / all).toFixed(1)
+    const positive = ((good / all) * 100).toFixed(1) + '%'
+    const statisticsArray = [good, neutral, bad, all, average, positive]
 
     return (
         <div>
@@ -46,7 +75,7 @@ const App = () => {
             <Button action={() => setGood(good + 1)} name="Good" />
             <Button action={() => setNeutral(neutral + 1)} name="Neutral" />
             <Button action={() => setBad(bad + 1)} name="Bad" />
-            <Statistics good={good} neutral={neutral} bad={bad} />
+            <History all={all} array={statisticsArray} />
         </div>
     )
 }
